759639bb2155e7f72e47bc7ea1ae500fc2c034bc
emanuel71jo <emanuel71jo@gmail.com>
2020-06-04 13:58:39 -0300

    correted

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        url_image: `http://localhost:3333/uploads/${item.image}`
+        url_image: `http://192.168.1.242:3333/uploads/${item.image}`
       };
     });
 
